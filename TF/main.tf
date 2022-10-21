633b16e6370d0058580708994bcd60a7dfa820ac
jay190199 <90500715+jay190199@users.noreply.github.com>
2022-10-20 20:18:29 -0400

    Add files via upload
    terraform code 
    Here, I created few resources such as ec2 , 2 ECR repos i.e. mysql and webapp.
    I also opened the ports of 8081,8082,8083 using tf.

---
+++ TF/main.tf
@@ -0,0 +1,110 @@
+#  Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Data source for AMI id
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+# Data source for availability zones in us-east-1
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Data block to retrieve the default VPC id
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Define tags locally
+
+# Reference subnet provisioned by 01-Networking 
+resource "aws_instance" "my_amazon" {
+  ami                    = data.aws_ami.latest_amazon_linux.id
+  instance_type          = lookup(var.instance_type, var.env)
+  key_name               = aws_key_pair.jayp355.key_name
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags =    {
+      Name = "jayp355Amazon-Linux"
+    }
+}
+
+
+# Adding SSH key to Amazon EC2
+resource "aws_key_pair" "jayp355" {
+  key_name   = "jayp355"
+  public_key = file("jayp355.pub")
+}
+
+# Security Group
+resource "aws_security_group" "my_sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  
+  ingress {
+    description      = "SSH from everywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description = "Http"
+    from_port   = 8083
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    description = "Http"
+    from_port   = 8082
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Http"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags ={
+      Name = "jayp355_sg"
+    }
+}
+
+resource "aws_ecr_repository" "webapp" {
+  name                 = "webapp"
+  image_tag_mutability = "MUTABLE"
+}
+
+resource "aws_ecr_repository" "mysql" {
+  name                 = "mysql"
+  image_tag_mutability = "MUTABLE"
+}
